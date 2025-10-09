af6c204f3a47c0c9f8f5b6b13405fec9b56358e9
Mariah Stutey <mariah.lynnn@icloud.com>
2025-10-08 20:19:05 -0700

    restart with new design

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const modal = document.getElementById('imageModal');
-const openPopup = document.getElementById('openPopup');
-const closeBtn = document.querySelector('.close');
-
-openPopup.addEventListener('click', () => modal.style.display = 'flex');
-closeBtn.addEventListener('click', () => modal.style.display = 'none');
-
